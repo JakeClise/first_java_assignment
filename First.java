186231356af4f86be43eabc33ee170881def1cb3
JakeClise <jhclise@gmail.com>
2023-02-06 19:58:23 -0500

    first commit

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Jake Clise");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Queenstown, MD");
+    }
+}
---
+++ Second.java
@@ -0,0 +1,10 @@
+public class Second {
+    public static void main(String[] args) {
+        String myName = "My name is Jake Clise";
+        String myAge = "I am 28 years old";
+        String myHomeTown = "My hometown is Queenstown, MD";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHomeTown);
+    }
+}
